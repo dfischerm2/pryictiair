50fdd3be7ba371cb71207ff5df9d331b30f3b27e
svera <svera@evecloudconsulting.com>
2024-10-16 16:34:47 -0500

    feat: se agrega email html para el envio de correos de invitación al congreso

--- notficar_leads_conference.py
+++ notficar_leads_conference.py
@@ -9,14 +9,25 @@ from landing.models import PersonNotificacion
 from pryictiair.settings import BASE_DIR
 from django.db import transaction
 
+from core.email_config import send_html_mail
+
 
 def notificar_conference():
     try:
         personas_nn = PersonNotificacion.objects.filter(notified=False)
         per_error_nn = []
         count = 1
+        subject = 'Call for Papers - International Conference on Technological Innovation and AI Research (ICTIAIR2025)'
+
         for person in personas_nn:
             try:
+                datos = {
+                    'persona': person,
+                }
+                to = person.email  # 'hllerenaa@unemi.edu.ec' #
+                send_html_mail(subject, "email/email_ictiair.html", datos, [to], [], [])
+                person.notified = True
+                person.save()
                 print(f"{count}.- Persona notificada: ({person.id}) {person.__str__()}")
                 count += 1
             except Exception as e:
